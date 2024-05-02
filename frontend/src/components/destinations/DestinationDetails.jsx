397f5992ef9e3abb6d6777c1ec643ea8f3ffcc12
DiegoRojo99 <diego99rojo@gmail.com>
2024-05-02 09:46:59 +0100

    Added children

--- frontend/src/components/destinations/DestinationDetails.jsx
+++ frontend/src/components/destinations/DestinationDetails.jsx
@@ -11,6 +11,16 @@ function DestinationDetails() {
   const [children, setChildren] = useState([]);
 
   useEffect(() => {
+    
+    function divideChildren(children){
+      let attractions = children.filter((child) => child.entityType === "ATTRACTION");
+      let restaurants = children.filter((child) => child.entityType === "RESTAURANT");
+      let hotels = children.filter((child) => child.entityType === "HOTEL");
+      let shows = children.filter((child) => child.entityType === "SHOW");
+
+      setChildren({ attractions, restaurants, hotels, shows});
+    }
+    
     const fetchData = async () => {
       try {
         const response = await fetch(`https://api.themeparks.wiki/v1/entity/${id}/children`);
@@ -19,7 +29,7 @@ function DestinationDetails() {
         }
 
         const result = await response.json();
-        setChildren(result.children.filter((child) => child.entityType === "PARK"));
+        divideChildren(result.children);
         setData(result);
         setLoading(false);
       } catch (error) {
@@ -65,16 +75,19 @@ function DestinationDetails() {
     navigate(url);
   }
 
-  function renderChildrenObjects() {
+  function renderChildrenObjects(type) {
+    if(!children[type].length){
+      return <></>;
+    }
     return (
       <div>
+        <div>
+          <h5>{type}</h5>
+        </div>
         <div className='grid-element'>
-          {children.map((child) => {
+          {children[type].map((child) => {
             return <Card child={child} openLink={openLink} />
           })}
-        </div>
-        <div>
-          
         </div>
       </div>
     );
@@ -83,10 +96,37 @@ function DestinationDetails() {
   return (
     <div>
       <div style={{ width: '100%', display: 'flex' }}>
+        <img src='.logo.png' />
+        <p>Attractions</p>
+        <p>Hotels</p>
+        <p>Explore Parks</p>
+        {/* Display hidden nav */}
+      </div>
+      <div style={{ width: '100%', display: 'flex' }}>
         <h1 style={{ margin: '32px 0', textAlign: 'center', width: '100%' }}>{data.name}</h1>
       </div>
+      <div className='park-header'>
+        <div className='breadcrumbs'>
+          {/* Breadcrumbs */}
+          <p style={{paddingLeft: '8px'}}> {"Home > Park"}  </p>
+        </div>
+        <div className='park-header-info'>
+          <div className='park-logo-hours'>
+            <img src=".\..\..\img\logo192.png" />
+            <p>Today's Park Hours: -am to -pm</p>
+            <a>Daily Hours</a>
+          </div>
+          <div style={{width: '40%'}}>
 
-      {renderChildrenObjects()}
+          </div>
+          <div style={{width: '30%'}} className='header-map'>
+
+          </div>
+        </div>
+      </div>
+
+      {renderChildrenObjects("attractions")}
+      {renderChildrenObjects("shows")}
     </div>
   );
 };
