39c39fee1d07aedcddf56f8c1d419e55f4a991bc
DiegoRojo99 <diego99rojo@gmail.com>
2024-05-20 20:44:23 +0100

    Updated nav

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -3,17 +3,16 @@ import './Nav.css';
 function Nav() {
   return (
     <div class="nav-menu">
+      <a className='nav-logo' href='/'>Park Hopper</a>
+      <div></div>
       <ul class="nav-menu-content">
-        <li><a href="/"><span class="material-symbols-outlined">home</span></a></li>
-        {/* <li><a href="#"><span class="material-symbols-outlined">dashboard</span><span>DashBoard</span></a></li> */}
-        <li><a href="/explore"><span class="material-symbols-outlined">explore</span></a></li>
-        <li><a href="#"><span class="material-symbols-outlined">settings</span></a></li>
-        {/* <li><a href="#"><span class="material-symbols-outlined">analytics</span><span>Analytics</span></a></li>
-        <li><a href="#"><span class="material-symbols-outlined">person</span><span>Account</span></a></li>
-        <li><a href="#"><span class="material-symbols-outlined">report</span><span>Report</span></a></li>
-        <li><a href="#"><span class="material-symbols-outlined">email</span><span>Contact</span></a></li>*/}
-        <li style={{flex: '16'}}></li>
-        <li style={{float: 'right'}}><a href="/login"><span class="material-symbols-outlined">login</span></a></li> 
+        <li><a href="/explore">Explore</a></li>
+        <li><a href="/settings">Settings</a></li>
+        <li><a href="/favs">Favs</a></li>
+        <li><a href="/login">Login/Logout</a></li>
+        {/* <li><a href="/explore"><span class="material-symbols-outlined">explore</span></a></li> */}
+        {/* <li><a href="#"><span class="material-symbols-outlined">settings</span></a></li>
+        <li style={{float: 'right'}}><a href="/login"><span class="material-symbols-outlined">login</span></a></li>  */}
       </ul>
     </div>
   );
