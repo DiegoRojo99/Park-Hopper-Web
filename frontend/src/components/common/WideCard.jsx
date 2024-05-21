a9e04f1f0a7ddcd1f5d45cd6fb59bf276f55957c
DiegoRojo99 <diego99rojo@gmail.com>
2024-05-21 11:09:46 +0100

    Updated List Wide Card

--- frontend/src/components/common/WideCard.jsx
+++ frontend/src/components/common/WideCard.jsx
@@ -2,14 +2,12 @@ import './Common.css';
 
 function WideCard({ child, openLink }){
   return (
-    <div className='wide-card'>
-      <div className='wide-card2' onClick={() => openLink(child.id)}>
-          {/* <img className='wide-card-img' src={logoImage} /> */}
-          <div className='overlay'>            
-            <p className='overlay-name'>{child.name}</p>
-            <p className='overlay-wait'>{child?.queue?.STANDBY?.waitTime ? child.queue.STANDBY.waitTime : "-"}</p>
-            <i style={{width: '5%'}}>⌚</i>
-          </div>
+    <div className='wide-card' onClick={() => openLink(child.id)}>
+      {/* <img className='wide-card-img' src={logoImage} /> */}
+      <div className='overlay'>            
+        <p className='overlay-name'>{child.name}</p>
+        <p className='overlay-wait'>{child?.queue?.STANDBY?.waitTime ? child.queue.STANDBY.waitTime : "-"}</p>
+        <i style={{width: '5%'}}>⌚</i>
       </div>
     </div>
   );
